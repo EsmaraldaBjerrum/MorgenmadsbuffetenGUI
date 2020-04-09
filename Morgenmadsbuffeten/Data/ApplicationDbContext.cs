aa0bda83017d195e950ea10bac8a62b20036d0fc
Esmaralda Dorthea Bjerrum <au593888@uni.au.dk>
2020-04-09 16:56:45 +0200

    Added breakfastOrder entity and added it to database

--- Morgenmadsbuffeten/Data/ApplicationDbContext.cs
+++ Morgenmadsbuffeten/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Morgenmadsbuffeten.Models;
 
 namespace Morgenmadsbuffeten.Data
 {
@@ -12,5 +13,6 @@ namespace Morgenmadsbuffeten.Data
             : base(options)
         {
         }
+        public DbSet<BreakfastOrder> BreakfastOrders { get; set; }
     }
 }
